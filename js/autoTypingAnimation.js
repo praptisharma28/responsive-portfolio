51d83bdced09c5b841bb0a164fbf48773ecf2966
praptisharma28 <123169861+praptisharma28@users.noreply.github.com>
2023-12-30 15:46:40 +0530

    changes

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,9 +1,9 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am Golam Morsed</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eius officia ea ut vonsectetur adipisicing elit. Illo voluptatum iste, tempora rem obcaecati omnis delectus sunt, consequuntur, et ut quasi aperiam assumenda! <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex beatae reprehenderit vero maiores dignissimos error vitae, optio inventore explicabo quia officiis magni delectus? Lorem ipsum dolor, sit amet consectetur"
+        "<span>Hi, I am Prapti Sharma</span> Expertise in both front-end and back-end. 
       ],
-      typeSpeed: 7,
+      typeSpeed: 4,
       contentType: 'html'
     });
   }, 100);
